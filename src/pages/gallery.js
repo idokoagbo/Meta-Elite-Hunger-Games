abab4a29d109c031c339689d586e075da73288d4
Idoko Agbo <idokojudge@gmail.com>
2022-02-26 05:31:26 -0500

    Initial commit

---
+++ src/pages/gallery.js
@@ -0,0 +1,8 @@
+
+function Gallery() {
+    return <div>
+        <h1>This is gallery</h1>
+    </div>;
+}
+
+export default Gallery;
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+function Index() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Meta Elite Hunger Games
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default Index;
