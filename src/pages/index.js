7a47f7fe309d15074909e3d0f4b7820d6c4d8315
Idoko Agbo <idokojudge@gmail.com>
2022-02-27 09:22:36 -0500

    add hero area, particles animation and navigation bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,33 @@
-import logo from '../logo.svg';
-import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../assets/logo.svg';
+import bsc from '../assets/binance-smart-chain.png';
+import '../assets/css/App.css';
+import Hero from '../components/hero';
+import Nav from '../components/nav';
 
 function Index() {
     return (
         <div className="App">
+            <Nav logo={logo} />
             <header className="App-header">
+                <Hero />
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Meta Elite Hunger Games
                 </p>
+                <p>
+                    <a className='btn btn-secondary btn-lg'>Seed Sale</a> &nbsp; <a className='btn btn-dark btn-lg'>Claim Airdrop</a>
+                </p>
+                <div className='row'>
+                    <div className='col'>
+                        <p>Designed with</p>
+                        <img src='https://raw.githubusercontent.com/Kryxivia/Kryxivia.Website/414432bd650657cb09f4952e64bd555af16aa78f/assets/img/unity.svg' height={50} />
+                    </div>
+                    <div className='col'>
+                        <p>Powered by</p>
+                        <img src={bsc} height={50} />
+                    </div>
+                </div>
             </header>
         </div>
     );
