cabfc4f3649d5716f15c35bda789cec4369cf2c0
Idoko Agbo <idokojudge@gmail.com>
2022-03-13 23:15:54 -0400

    add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/css/App.css';
+import Nav from '../components/nav';
+import logo from '../assets/logo.png';
+import Top from '../assets/top-ix.png';
+import Bottom from '../assets/bottom-ix.png';
+
 function About() {
-    return <div>
-        <h1>This is about page</h1>
-    </div>
+    return <>
+        <Nav logo={logo} />
+        <img src={Top} className="img-fluid bg-top" />
+        <section className='character-area'>
+            <div className='container text-center'>
+                <hr className='about-title' />
+            </div>
+
+        </section>
+        <img src={Bottom} className="img-fluid bg-bottom" />
+    </>;
 }
 
 export default About;
