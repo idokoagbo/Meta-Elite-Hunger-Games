08089e59de2fd9e29c70905410edb4a8fee9f67d
Idoko Agbo <idokojudge@gmail.com>
2022-04-12 07:42:01 -0400

    add gallery page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,8 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/css/App.css';
+import Nav from '../components/nav';
+import logo from '../assets/logo.png';
+import Top from '../assets/backgrounds/top.png';
+import Bottom from '../assets/backgrounds/bottom.png';
 
 function Gallery() {
-    return <div>
-        <h1>This is gallery</h1>
-    </div>;
+    return <>
+        <Nav logo={logo} />
+        <img src={Top} className="img-fluid bg-top" />
+        <section className='character-area'>
+            <div className='container text-center'>
+                <hr className='gallery-title' />
+            </div>
+
+        </section>
+        <img src={Bottom} className="img-fluid bg-bottom" />
+    </>;
 }
 
 export default Gallery;
