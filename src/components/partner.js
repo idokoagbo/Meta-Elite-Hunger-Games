40d4e5eddd2cd8335164242ad3e0b3a8ad78916d
Idoko Agbo <idokojudge@gmail.com>
2022-04-18 07:21:07 -0400

    Hotfix: animate partners

--- src/components/partner.js
+++ src/components/partner.js
@@ -1,5 +1,5 @@
 function Partner(props) {
-    return <div className='col-md-3 mt-3'>
+    return <div className='col-md-3 mt-3 animate__animated animate__fadeInLeft animate__delay-5s animate__slow'>
         <div className="container partner-img" >
             <img src={props.img}/>
         </div>
